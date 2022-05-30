06c6f14f0de968348069897e788c0ccf44a287b4
Erkan Çalışkan <erkancaliskan120@hotmail.com>
2022-05-30 18:28:12 +0300

    Versiov v1.3

--- NewLinkShortApp/Controllers/SvgConvertPdfController.cs
+++ NewLinkShortApp/Controllers/SvgConvertPdfController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Text;
-using System.IO;
 using NewLinkShortApp.Models;
+using System.IO;
 
 namespace NewLinkShortApp.Controllers
 {
@@ -18,29 +18,43 @@ namespace NewLinkShortApp.Controllers
         {
             return View();
         }
+
+        private string TemplatePath()
+        {
+            string path = System.AppContext.BaseDirectory;
+            path=Path.Combine(path, "Templates\\deneme.svg");
+            return path;
+        }
+        private string CertificatePath(string fileName)
+        {
+            string path = System.AppContext.BaseDirectory;
+            //string path2 = AppDomain.CurrentDomain.BaseDirectory;
+            //string path3 = Directory.GetCurrentDirectory();
+            //string path4 = Environment.CurrentDirectory;
+            //string path5 = this.GetType().Assembly.Location;
+            //var pat = AppDomain.CurrentDomain.RelativeSearchPath;
+            path =Path.Combine(path, "Certificates",fileName);
+            
+            return path;
+        }
         [HttpPost]
         public ActionResult Index(Certificate p)
         {
 
-            string fileName = @"C:\Users\erkan.caliskan\source\repos\NewLinkShortApp\NewLinkShortApp\Text\certificate.txt";
-            
-            //string text = File.ReadAllText(fileName);
-            
-            //text = text.Replace("some text", p.Name);
-            //text = text.Replace("some text", p.Surname);
-            //text = text.Replace("some text", p.Tarih.ToLongDateString());
-            
-            //File.WriteAllText(fileName, text);
+            //string fileName = @"C:\Users\erkan.caliskan\source\repos\NewLinkShortApp\NewLinkShortApp\Templates\deneme.svg";
+            string fileName = TemplatePath();
 
+  
 
-            //string text = "";
+            string text = System.IO.File.ReadAllText(fileName);
 
-            //using (StreamReader streamReader = new StreamReader(fileName, Encoding.ASCII, true))
-            //{
-            //    text = File.ReadAllText(fileName);
-            //    Console.WriteLine(text);
-            //}
+            text = text.Replace("{Text1}", p.Name);
+            text = text.Replace("{Text2}", p.Surname);
+            text = text.Replace("{Text3}", p.Tarih.ToString("s"));
 
+            var certicateName = CertificatePath(DateTime.Now.ToString("ddMMyyyyHHmmss") +".svg");
+
+            System.IO.File.WriteAllText(certicateName,text);
 
 
             return RedirectToAction("SvgPdf", "SvgConvertPdf");
